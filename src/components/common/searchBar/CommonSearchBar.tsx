a1167bed823e5c852e1eb2eb64639e0834c8147f
seodaekyu <grarlove7@naver.com>
2024-05-31 17:48:22 +0900

    검색 기능 구현

--- src/components/common/searchBar/CommonSearchBar.tsx
+++ src/components/common/searchBar/CommonSearchBar.tsx
@@ -1,14 +1,43 @@
+import { useState } from 'react'
+import { useRecoilState } from 'recoil'
+import { searchState } from '@/recoil/atoms/searchState'
 import styles from './CommonSearchBar.module.scss'
 
 function CommonSearchBar() {
-  return (
-    <div className={styles.searchBar}>
-        <div className={styles.searchBar__search}>
-            <input type="text" placeholder='찾으실 이미지를 검색해주세요.' className={styles.searchBar__search__input} />
-            <img src="src/assets/icons/icon-search.svg" alt="" />
-        </div>
-    </div>
-  )
+	const [search, setSearch] = useRecoilState(searchState)
+	const [text, setText] = useState("")
+	const onChange = (event) => {
+		console.log(event.target.value)
+		setText(event.target.value)
+	}
+	const onSearch = () => {
+		if(text === "") {
+			// input 태그 안에 빈 값을 검색하였을때 => searching default value
+			setSearch("Korea")
+		} else {
+			setSearch(text) // 작성한 Input Value 값 할당
+		}
+	}
+
+	const handleKeyDown = (event: React.KeyboardEvent) => {
+		if(event.key === "Enter") {
+			if(text === "") {
+				// input 태그 안에 빈 값을 검색하였을때 => searching default value
+				setSearch("Korea")
+			} else {
+				setSearch(text) // 작성한 Input Value 값 할당
+			}
+		}
+	}
+
+	return (
+		<div className={styles.searchBar}>
+			<div className={styles.searchBar__search}>
+				<input type="text" placeholder='찾으실 이미지를 검색해주세요.' className={styles.searchBar__search__input} value={text} onChange={onChange} onKeyDown={handleKeyDown} />
+				<img src="src/assets/icons/icon-search.svg" alt="" onClick={onSearch} />
+			</div>
+		</div>
+	)
 }
 
 export default CommonSearchBar
